9d0a6d59098bd021236841be7152a97b895e5f96
libaibaibai <1643843924@qq.com>
2018-04-20 14:53:04 +0800

    提交

---
+++ gitwendang/src/com/bao/contoller/UserAction.java
@@ -0,0 +1,13 @@
+package com.bao.contoller;
+
+import com.bao.entity.User;
+
+public class UserAction  {
+
+	
+	public static void main(String[] args) {
+		 User u = new User();
+		 u.setName("asd");
+		
+	}
+}
